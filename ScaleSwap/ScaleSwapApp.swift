6974d2dab4ee9804c4639f4c1d3d66bed309b696
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-12-03 21:40:34 +0100

    Initial Commit

---
+++ ScaleSwap/ScaleSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleSwapApp.swift
+//  ScaleSwap
+//
+//  Created by Wojuade Abdul Afeez on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScaleSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
